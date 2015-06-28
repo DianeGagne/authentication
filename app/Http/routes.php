0cfea79558eaca3301849cb6c58cc1bdc68486d7
Diane <drose.gagne@gmail.com>
2015-06-28 17:05:41 -0600

    added routes and controller for first three basic pages

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('signin', 'SignInController@signin');
+Route::get('loggedin', 'SignInController@loggedin');
+Route::get('signup', 'SignInController@signup');
