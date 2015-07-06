32e02c2ac8a727c13280095f5bac925f54172be1
vagrant <vagrant@homestead>
2015-07-06 00:45:17 +0000

    added some simple test cases for the redirect when not logged in

---
+++ tests/LoginRedirectTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\User;
+
+class LoginRedirectTest extends TestCase
+{
+
+    /**
+     * the loggedin page should only be available when we are authenticated
+     * When we are not, we should redirect to the sign in page
+     */
+    public function testNotLoggedInRedirect()
+    {
+        $response = $this->call('GET', 'loggedin');
+
+        $this->assertRedirectedToRoute('login');
+
+        return 'Tested redirect';
+    }
+
+    /**
+     * Likewise if we are logged in, should go to the logged in page
+     */
+    public function testLoggedInRedirect()
+    {
+        $user = new User(array('name' => 'Diane'));
+        $this->be($user); //You are now authenticated
+
+        $response = $this->call('GET', 'loggedin');
+        $this->see('Logged in');
+        return 'tested login';
+    }
+}
